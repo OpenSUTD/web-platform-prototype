42856d37a251d4850aa2bec23c50a55d4cf15c8f
Bhavy Mital <bhavy3110@gmail.com>
2019-03-05 20:23:30 +0800

    Change Django user model to our custom one

--- projects/admin.py
+++ projects/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import *
 
 # Register your models here.
 
+admin.site.register(User, UserAdmin)
+
 admin.site.register(Project)
-admin.site.register(User)
+#admin.site.register(User)
 
 admin.site.register(Tag)

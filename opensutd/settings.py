f379125636cc7af74495c3c7e142d48fffb7bd9f
Ning Si Kai <notusingrealname@gmail.com>
2019-03-22 02:25:08 +0800

    Fixed navbar,  login and logout links

--- opensutd/settings.py
+++ opensutd/settings.py
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
--- projects/urls.py
+++ projects/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'projects'
 
 urlpatterns = [
-    path('', views.index),
-    path('users/<username>/', views.user_view, name="user"),
+    path('', views.index, name='home'),
+    path('users/<user_id>/', views.user_view, name="user"),
     path('projects/', views.projects_list_view, name="projects_list"),
     path('projects/<project_uid>/', views.project_view, name="showcase"),
     path('admin/approve', views.approval_view, name='approval')

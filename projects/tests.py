646b0aa4f860c7b638f06dda82d45fc66f25e831
Timothy Liu <tlkh.xms@gmail.com>
2019-03-22 09:30:22 +0800

    fix some of the test cases

--- projects/tests.py
+++ projects/tests.py
@@ -12,23 +12,23 @@ client = Client()
 
 class UserTestCase(TestCase):
     def setUp(self):
-        User.objects.create(user_id="tom",
+        User.objects.create(username="tom",
                             display_name="Tom Magnanti",
                             graduation_year=2018,
                             pillar="ISTD")
 
-        User.objects.create(user_id="jane",
+        User.objects.create(username="jane",
                             display_name="Jane Tan",
                             graduation_year=2021,
                             pillar="ESD")
 
     def test_user_basic_info(self):
-        tom = User.objects.get(user_id="tom")
+        tom = User.objects.get(username="tom")
         self.assertEqual(tom.pillar, "ISTD")
         self.assertEqual(tom.graduation_year, 2018)
         self.assertEqual(tom.display_name, "Tom Magnanti")
 
-        jane = User.objects.get(user_id="jane")
+        jane = User.objects.get(username="jane")
         self.assertEqual(jane.pillar, "ESD")
         self.assertEqual(jane.graduation_year, 2021)
         self.assertEqual(jane.display_name, "Jane Tan")
@@ -65,19 +65,19 @@ class ProjectShowcaseTestCase(TestCase):
                                 url="https://github.com/OpenSUTD/web-platform-prototype",
                                 status="ACCEPT")
 
-        User.objects.create(user_id="tom",
+        User.objects.create(username="tom",
                             display_name="Tom Magnanti",
                             graduation_year=2018,
                             pillar="ISTD")
 
-        User.objects.create(user_id="jane",
+        User.objects.create(username="jane",
                             display_name="Jane Tan",
                             graduation_year=2021,
                             pillar="ESD")
         
     def test_add_user_project(self):
-        tom = User.objects.get(user_id="tom")
-        jane = User.objects.get(user_id="jane")
+        tom = User.objects.get(username="tom")
+        jane = User.objects.get(username="jane")
 
         proj = Project.objects.get(project_uid="ACAD_00001")
         proj.users.add(tom)
@@ -86,8 +86,8 @@ class ProjectShowcaseTestCase(TestCase):
         self.assertEqual(len(proj.users.all()), 2)
 
     def test_add_del_user_project(self):
-        tom = User.objects.get(user_id="tom")
-        jane = User.objects.get(user_id="jane")
+        tom = User.objects.get(username="tom")
+        jane = User.objects.get(username="jane")
 
         proj = Project.objects.get(project_uid="ACAD_00001")
         proj.users.add(tom)

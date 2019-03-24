e6707b99df631eb2d2f22f4d2675a5a9f7538f00
Timothy Liu <tlkh.xms@gmail.com>
2019-03-24 22:35:49 +0800

    Fix login and admin

--- opensutd/settings.py
+++ opensutd/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.github',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- opensutd/urls.py
+++ opensutd/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('projects.urls')),
+    path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('projects.urls')),
 ]
